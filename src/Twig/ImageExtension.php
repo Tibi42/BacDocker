81a119a5f0f3f1223d9b4976ae0bacdb33a39f73
Guillaume PECQUET <guillaume.pecquet@gmail.com>
2026-07-19 03:54:43 +0200

    MAJ performence lighthouse

--- src/Twig/ImageExtension.php
+++ src/Twig/ImageExtension.php
@@ -2,6 +2,7 @@
 
 namespace App\Twig;
 
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 
@@ -10,10 +11,17 @@ use Twig\TwigFilter;
  */
 final class ImageExtension extends AbstractExtension
 {
+    public function __construct(
+        #[Autowire('%kernel.project_dir%')]
+        private readonly string $projectDir,
+    ) {
+    }
+
     public function getFilters(): array
     {
         return [
             new TwigFilter('prefer_webp', $this->preferWebp(...)),
+            new TwigFilter('responsive_small', $this->responsiveSmall(...)),
         ];
     }
 
@@ -27,5 +35,23 @@ final class ImageExtension extends AbstractExtension
         }
 
         return (string) preg_replace('/\.(jpe?g|png)$/i', '.webp', $filename);
+    }
+
+    /**
+     * Retourne le nom de la variante "-480" du fichier si elle existe réellement
+     * sur le disque (images/{$subdir}/), sinon null. Évite de générer un srcset
+     * pointant vers une ressource manquante pour des images uploadées via l'admin
+     * qui n'ont pas de variante réduite pré-générée.
+     */
+    public function responsiveSmall(?string $filename, string $subdir): ?string
+    {
+        if ($filename === null || $filename === '' || !str_ends_with(strtolower($filename), '.webp')) {
+            return null;
+        }
+
+        $small = substr($filename, 0, -\strlen('.webp')) . '-480.webp';
+        $path = $this->projectDir . '/public/images/' . $subdir . '/' . $small;
+
+        return is_file($path) ? $small : null;
     }
 }
